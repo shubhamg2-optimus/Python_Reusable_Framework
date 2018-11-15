27db21ff85452bc82bd17202cedba029cec3c69c
shubhamg2-optimus <shubham.goel.2@optimusinfo.com>
2018-11-15 16:27:43 +0530

    Commit with empty_email_field_validation_msg

--- pages/Amazon/sign_in_page.py
+++ pages/Amazon/sign_in_page.py
@@ -9,15 +9,21 @@ class SignInPage():
     email_text_field_xpath = "//input[@name='email']"
     password_text_field_xpath = "//input[@name='password']"
     continue_button_class = "a-button-input"
+    empty_email_validation_msg_xpath = ".//*[@id='auth-email-missing-alert']/div/div"
 
 
     # Constants
     duplicate_username_error_message = 'An account already exists with this username.'
     duplicate_email_message = "An account already exists with this email."
+    empty_email_validation_msg = "Enter your email or mobile phone number"
 
     # Functions
     def login_with_email_password(self, email, password):
         self.bp.enter_value_in_text_field_by_xpath(self.email_text_field_xpath, email)
         self.bp.element_click_by_class(self.continue_button_class)
         self.bp.enter_value_in_text_field_by_xpath(self.password_text_field_xpath, password)
+        self.bp.element_click_by_class(self.continue_button_class)
+
+    def login_with_empty_email_id(self, email):
+        self.bp.enter_value_in_text_field_by_xpath(self.email_text_field_xpath, email)
         self.bp.element_click_by_class(self.continue_button_class)
--- web_tests/Amazon_tests/test_amazon.py
+++ web_tests/Amazon_tests/test_amazon.py
@@ -6,3 +6,9 @@ class TestAccountDetails(BaseTest):
     def test_001_login_with_valid_credentials(self):
         self.hp.navigate_to_sign_in_page()
         self.sip.login_with_email_password("amazon14nov2018@gmail.com", "Amazon123")
+
+    def test_003_empty_username_field(self):
+        self.hp.navigate_to_sign_in_page()
+        self.sip.login_with_empty_email_id("")
+        act_validation_msg = self.bp.get_text(self.sip.empty_email_validation_msg_xpath)
+        self.assertEqual(act_validation_msg, self.sip.empty_email_validation_msg, "Invalid Error message")
