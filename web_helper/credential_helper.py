f4c86119941e2151cf2efd61f971bc6e54b752aa
shubhamg2 <goelshubham340@gmail.com>
2018-11-22 11:46:46 +0530

    commit with some changes

--- web_helper/credential_helper.py
+++ web_helper/credential_helper.py
@@ -4,10 +4,13 @@ from web_helper.general_utils import GeneralUtility
 class CredentialHelper():
 
     # EmailId
-    valid_email = GeneralUtility.email_generator()
+    #valid_email = GeneralUtility.email_generator()
+    valid_email = "amazon14nov2018@gmail.com"
 
     # Password
-    valid_password = "Amazon" + GeneralUtility.num_string_generator(3)
+    # valid_password = "Amazon" + GeneralUtility.num_string_generator(3)
+    valid_password = "Amazon123"
 
     # Username
-    username = "test_amazon_" + GeneralUtility.num_string_generator(2)
+    #username = "test_amazon_" + GeneralUtility.num_string_generator(2)
+    username = "Test"
